0fdbe7d51e3a8c1ae2b24dffeac65ea878d4d573
jgan96 <bird.brains.jason@gmail.com>
2017-10-08 14:03:23 -0400

    Update control.py

--- control.py
+++ control.py
@@ -1,5 +1,6 @@
 from gpiozero import *
 from time import sleep
+import argparse
 
 class Computer:
   def __init__(self, pin):
@@ -15,3 +16,7 @@ class Computer:
     sleep(15)
     self.off()
 
+parser = argparse.ArgumentParser(description='Turn on/off computers.')
+parser.add_argument('computer', help='Computer to turn on/off.')
+parser.add_argument('state', type=int, help='Use 1 for on and 0 for off')
+args = parser.parse_args()
