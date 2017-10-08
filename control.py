1411a23653d45aaec1461b92e3c563576f682f19
jgan96 <bird.brains.jason@gmail.com>
2017-10-08 15:07:50 -0400

    Update control.py

--- control.py
+++ control.py
@@ -16,7 +16,21 @@ class Computer:
     sleep(15)
     self.off()
 
-parser = argparse.ArgumentParser(description='Turn on/off computers.')
-parser.add_argument('computer', help='Computer to turn on/off.')
-parser.add_argument('state', type=int, help='Use 1 for on and 0 for off')
+//initialize computers to GPIO pins
+compA = Computer(10)
+compB = Computer(11)
+    
+//arg parser
+//TODO allow multiple computers to be turned on/off
+parser = argparse.ArgumentParser(description = 'Turn on/off computers.')
+parser.add_argument('computer', help = 'Computer(s) to turn on/off.')
+parser.add_argument('state', type = int, help = 'Use 1 for on and 0 for off')
 args = parser.parse_args()
+
+if(state == 0)
+  if(computer.find('101') != -1) compA.shutoff()
+  if(computer.find('102') != -1) compB.shutoff()
+if(state == 1)
+  if(computer.find('101') != -1) compA.boot()
+  if(computer.find('102') != -1) compB.boot()
+  
